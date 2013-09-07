d0dd52235f4aa43f817f48aa911e73b14e39ebf4
Alex Gorban <alex.gorban@gmail.com>
2013-09-07 11:52:30 -0500

    Fix typos.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 
 BUILD="_build"
 BUCKET="rvtrip.us"
 
+rm -rf $BUILD
 jekyll build --destination $BUILD
-s3cmd sync -P $BUILD s3://$BUCKET
+s3cmd sync -fP --delete-removed $BUILD/ s3://$BUCKET/
 s3cmd put -P -m "text/css" $BUILD/assets/css/main.css s3://$BUCKET/assets/css/main.css
